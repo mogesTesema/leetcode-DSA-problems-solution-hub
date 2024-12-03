7f908f4e0744aeceb143eb612461cb26166d8929
Moges-Tesema <moges2421@gmail.com>
2024-12-03 16:40:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.4 MB (99.96%)

---
+++ 0043-multiply-strings/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        num1=int(num1)
+        num2=int(num2)
+        num=num1*num2
+        num=str(num)
+        return num
+        
