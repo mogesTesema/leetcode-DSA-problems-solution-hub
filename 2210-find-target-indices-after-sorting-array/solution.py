9748d2dd5c2f367f35a712d9881218c0f2554e6a
Moges-Tesema <moges2421@gmail.com>
2025-04-25 13:40:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (16.14%)

---
+++ 2210-find-target-indices-after-sorting-array/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        answer = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] == target:
+                answer.append(i)
+        return answer
