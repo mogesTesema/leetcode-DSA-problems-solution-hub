e5219dab6cb1494e562211bc61ef14f1553d8533
Moges-Tesema <moges2421@gmail.com>
2025-04-29 13:20:20 +0000

    [LeetCode Sync] Runtime - 732 ms (5.02%), Memory - 18.2 MB (75.45%)

---
+++ 2502-sort-the-people/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        for i in range(len(names)-1):
+            for j in range(0, len(names)-i-1):
+                if heights[j]<heights[j+1]:
+                    heights[j],heights[j+1]= heights[j+1], heights[j]
+                    names[j], names[j+1] = names[j+1], names[j]
+        return names
+
+
+
+        
