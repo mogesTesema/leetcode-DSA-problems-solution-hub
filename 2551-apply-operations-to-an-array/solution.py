f1bd6e1f5be1a1148f19939b930809894dc971dd
Moges-Tesema <moges2421@gmail.com>
2025-04-30 20:23:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.1 MB (6.81%)

---
+++ 2551-apply-operations-to-an-array/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *=2
+                nums[i+1] = 0
+        non_zero = [num for num in nums if num != 0]
+        zeros = [num for num in nums if num == 0]
+        return non_zero + zeros
