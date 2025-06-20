e5fe4e7b8965c1899066dcdc64a85762341d1ebf
Moges-Tesema <moges2421@gmail.com>
2025-06-20 07:21:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (12.41%)

--- 0144-binary-tree-preorder-traversal/solution.py
+++ 0144-binary-tree-preorder-traversal/solution.py
@@ -6,18 +6,29 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # ans = []
+        # rights =[]
+        # head = root
+        # while head or rights:
+        #     if head:
+        #         ans.append(head.val)
+        #         if head.right:
+        #             rights.append(head.right)
+        #         head = head.left
+        #     else:
+        #         head = rights.pop()
+        # return ans
         ans = []
-        rights =[]
-        head = root
-        while head or rights:
-            if head:
-                ans.append(head.val)
-                if head.right:
-                    rights.append(head.right)
-                head = head.left
-            else:
-                head = rights.pop()
+        def preorder(root):
+            if root == None:
+                return
+            ans.append(root.val)
+            preorder(root.left)
+            preorder(root.right)
+        preorder(root)
         return ans
+
+
 
 
         
