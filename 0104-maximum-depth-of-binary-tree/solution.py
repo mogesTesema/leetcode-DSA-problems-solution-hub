30d8888977fbd0ff44eb0b2b2de5f1f59c101bf0
Moges-Tesema <moges2421@gmail.com>
2025-06-20 13:27:30 +0000

    [LeetCode Sync] Runtime - 1 ms (45.28%), Memory - 19.2 MB (29.88%)

--- 0104-maximum-depth-of-binary-tree/solution.py
+++ 0104-maximum-depth-of-binary-tree/solution.py
@@ -6,17 +6,30 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        res = 0
-        stack = []
-        stack.append((root,1))
-        while stack:
-            node, depth = stack.pop()
-            if node:
-                res = max(res, depth)
-                stack.append((node.right, depth+1))
-                stack.append((node.left, depth+1))
-        return res
+        # iterative approach using stack.
+        # if not root:
+        #     return 0
+        # longest = 0
+        # stack = []
+        # stack.append((root,1))
+        # while stack:
+        #     node, depth = stack.pop()
+        #     if node:
+        #         longest = max(longest, depth)
+        #         stack.append((node.right, depth+1))
+        #         stack.append((node.left, depth+1))
+        # return longest
+        ans = [0]
+        def depthSeeker(root,depth):
+            if root == None:
+                return
+            depth += 1
+            depthSeeker(root.left,depth)
+            depthSeeker(root.right,depth)
+            if root.right == None and root.left == None:
+                ans.append(depth)
+        depthSeeker(root,0)
+        return max(ans)
+
 
         
