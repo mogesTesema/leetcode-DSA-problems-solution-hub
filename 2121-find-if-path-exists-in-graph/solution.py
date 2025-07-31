38b84f9bc55a1c62fdcbec148865b537ec60f2bc
Moges-Tesema <moges2421@gmail.com>
2025-07-31 15:19:44 +0000

    [LeetCode Sync] Runtime - 572 ms (50.02%), Memory - 140.1 MB (16.00%)

--- 2121-find-if-path-exists-in-graph/solution.py
+++ 2121-find-if-path-exists-in-graph/solution.py
@@ -1,33 +1,6 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        """
-        how to approach the problem?
-        1.understand the problem
-            a. input
-            b. output
-            c. relation between input and output
-        2.explore with concerate example
-            a. simple test
-            b. edge test
-            c. input input
-            d. invalid input
-
-        3.break it down
-            a. make simplified version without thinking about syntax
-                1. change input into adjacency list
-                    a. create graph array that will contian each vertix's neighborers vertices.
-                    b. iterate through input, add to graph list with bi-direction
-                
-                2.starting from source node route to destination node using DFS
-                    a. create visited list and pathFound variable with False
-
-                    b. recursivly visit node and check for distination
-            c. make high level psdocode
-            
-        4.solve and simplify
-        5. refactore
-            a. you have solved but not done.
-        """
+    
     
         # sub solution one
         graph = defaultdict(list)
