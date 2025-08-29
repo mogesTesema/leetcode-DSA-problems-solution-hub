7df76807ed9a847fe4cd532973dfc8ba585fac1a
Moges-Tesema <moges2421@gmail.com>
2025-08-29 20:35:39 +0000

    [LeetCode Sync] Runtime - 235 ms (96.17%), Memory - 84.4 MB (90.04%)

--- 2121-find-if-path-exists-in-graph/solution.py
+++ 2121-find-if-path-exists-in-graph/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
     
-    
-        # sub solution one
+        """
+        # naive graph traversal solution
         graph = defaultdict(list)
         for v,u in edges:
             graph[v].append(u)
@@ -22,3 +22,26 @@ class Solution:
             return False
 
         return DFS(source)
+        """
+        # union-find version of problem solution
+        roots = [i for i in range(n)]
+        rank = [1]*n
+        def find(i):
+            if i == roots[i]: return i
+            roots[i] = find(roots[i])
+            return roots[i]
+
+        def union(i,j):
+            r1, r2 = find(i), find(j)
+            if r1 == r2: return
+            if rank[r1] > rank[r2]:
+                roots[r2] = r1
+            elif rank[r1] < rank[r2]:
+                roots[r1] = r2
+            else:
+                roots[r1] = r2
+                rank[r2] += 1
+                
+        for root1, root2 in edges:
+            union(root1,root2)
+        return find(source) == find(destination)
