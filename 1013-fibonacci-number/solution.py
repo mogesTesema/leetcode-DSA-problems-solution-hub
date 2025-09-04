302efe49ca7b9472644333123335121d47cd68fc
Moges-Tesema <moges2421@gmail.com>
2025-09-04 20:25:39 +0000

    [LeetCode Sync] Runtime - 37 ms (72.41%), Memory - 17.9 MB (23.38%)

--- 1013-fibonacci-number/solution.py
+++ 1013-fibonacci-number/solution.py
@@ -2,11 +2,22 @@ class Solution:
     def __init__(self):
         self.fibValues = {}
     def fib(self, n: int) -> int:
-        if n == 1: return 1
-        if n == 0: return 0
-        if n in self.fibValues: return self.fibValues[n]
-        untracked = self.fib(n-1) + self.fib(n-2)
-        self.fibValues[n] = untracked
-        return untracked
+
+        # if n == 1: return 1
+        # if n == 0: return 0
+        # if n in self.fibValues: return self.fibValues[n]
+        # untracked = self.fib(n-1) + self.fib(n-2)
+        # self.fibValues[n] = untracked
+        # return untracked
+        
+        memo = defaultdict(int)
+        
+        def fib(n):
+            if n == 0: return 0
+            if n == 1: return 1
+            if n not in memo:
+                memo[n] = fib(n-1) + fib(n-2)
+            return memo[n]
+        return fib(n)
     
         
