3dc75b092141fe2692160a3b961a7112770d93f2
Moges-Tesema <moges2421@gmail.com>
2025-11-14 07:32:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (62.89%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,11 +1,46 @@
-from typing import List
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
-        return [-1, -1]
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
+
+        # nums_dict = {val:index for index,val in enumerate(nums)}
+        # print(f"original dictionary: {nums_dict}")
+        # nums_tuples= list(nums_dict.keys())
+        # print(f"value of dictionary:{keys}")
+        # keys.sort()
+
+
+        # nums_tuples =[]
+        # for index, val in enumerate(nums):
+        #     nums_tuples.append((val,index))
+        # # print(nums_tuples)
+        # nums_tuples.sort()
+        # # print(nums_tuples)
+        # left = 0 
+        # right = len(nums_tuples) -1
+        # while left < right:
+        #     # print(f"left:{left},right:{right}")
+        #     current_sum = nums_tuples[left][0] + nums_tuples[right][0] 
+        #     # print(f"current_sum: {current_sum},targe:{target}")
+
+        #     if current_sum == target:
+        #         index_one = nums_tuples[left][1]
+        #         index_two = nums_tuples[right][1]
+        #         return [index_one,index_two]
+
+        #     elif current_sum < target:
+        #         left += 1
+
+        #     else:
+        #         right -= 1
+        nums_dict =dict()
+        for index, val in enumerate(nums):
+            diff = target - val
+            if diff in nums_dict:
+                return [nums_dict[diff], index]
+            else:
+                nums_dict[val] = index
+            
+
