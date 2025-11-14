8bac79aceea8c769aecd84ec3029dd5f9cc38c43
Moges-Tesema <moges2421@gmail.com>
2025-11-14 08:16:51 +0000

    [LeetCode Sync] Runtime - 103 ms (44.39%), Memory - 27 MB (32.84%)

--- 0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,11 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy = sell = profit = 0
-        for i in range(len(prices)):
-            if prices[i] < prices[buy]:
-                buy = i
-                sell = i
-            if prices[i] > prices[sell]:
-                sell = i
-            profit = max(profit, prices[sell]-prices[buy])
-        return profit
+        max_profit = -float("inf")
+        days = len(prices)
+        # for i in range(days):
+        #     for j in range(i+1,days):
+        #         max_profit = max(prices[j]-prices[i], max_profit)
+        # return max_profit if max_profit > 0 else 0
+        buy = 0
+        sell = 1
+        while sell < days:
+            current_profit = prices[sell] - prices[buy]
+            max_profit = max(max_profit,current_profit)
+            if current_profit < 0:
+                buy = sell
+            sell += 1
+        return max_profit if max_profit > 0 else 0
+            
+
