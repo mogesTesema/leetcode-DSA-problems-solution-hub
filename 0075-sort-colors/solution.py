8668a6e6a7a92afc3675218e8468d50fab0e961f
Moges-Tesema <moges2421@gmail.com>
2025-11-29 16:37:44 +0000

    [LeetCode Sync] Runtime - 4 ms (5.84%), Memory - 17.8 MB (33.63%)

--- 0075-sort-colors/solution.py
+++ 0075-sort-colors/solution.py
@@ -3,11 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        colors = defaultdict(int)
-        for color in nums:
-            colors[color] = colors.get(color,0) +1
-        print(colors)
-        nums[:] = []
-        for i in range(3):
-            nums.extend([i]*colors[i])
+        frq = defaultdict()
+        for num in nums:
+            frq[num] = frq.get(num,0) + 1
+        
+        nums_two = []
+        if 0 in frq:
+            nums_two.extend([0]*frq[0])
+        if 1 in frq:
+            nums_two.extend([1]*frq[1])
+        if 2 in frq:
+            nums_two.extend([2]*frq[2])
+        for index,num in enumerate(nums_two):
+            nums[index] = num
+            
         print(nums)
