dfed68bc7b65aceb5229fa5f79521cacc6eb1fcd
Moges-Tesema <moges2421@gmail.com>
2025-12-09 06:41:49 +0000

    [LeetCode Sync] Runtime - 14 ms (68.76%), Memory - 17.7 MB (70.28%)

--- 0003-longest-substring-without-repeating-characters/solution.py
+++ 0003-longest-substring-without-repeating-characters/solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left = max_length = 0
-        char_set = set()
-        
-        for right in range(len(s)):
-            while s[right] in char_set:
-                char_set.remove(s[left])
-                left += 1
+        window_set = set()
 
-            char_set.add(s[right])
-            max_length = max(max_length, right - left + 1)
-        
+        left = 0
+        max_length = 0
+
+        for right in range(len(s)):
+            while s[right] in window_set:
+                window_set.remove(s[left])
+                left += 1
+            window_set.add(s[right])
+
+            max_length = max(max_length,right - left + 1)
         return max_length
+        
