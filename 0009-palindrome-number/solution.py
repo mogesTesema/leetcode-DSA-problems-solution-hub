141762888ad20e250c66ce06b9f7613a38498c00
Moges-Tesema <moges2421@gmail.com>
2026-02-06 07:22:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (33.55%)

--- 0009-palindrome-number/solution.py
+++ 0009-palindrome-number/solution.py
@@ -1,17 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        isPalin=False
-        if x<0:
-            return False
-
-        y=str(x)
-        z=""
-        l=len(y)
-        for i in range(l):
-            z+=y[l-i-1]
-        y=int(z)
-        if(x==y):
-            isPalin=True
-        return isPalin
-        
+        x = str(x)
+        return x == x[::-1]
         
