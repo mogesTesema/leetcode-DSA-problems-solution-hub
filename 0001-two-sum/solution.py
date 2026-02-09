9e7f83484f2db0813c2d90671c4c11c0d5cd419f
Moges-Tesema <moges2421@gmail.com>
2026-02-09 14:30:15 +0000

    [LeetCode Sync] Runtime - 8 ms (34.69%), Memory - 21 MB (5.07%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,46 +1,24 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return [i,j]
+        elem = []
+        for index,num in enumerate(nums):
+            elem.append([num,index])
+        
+        elem.sort()
+       
+        left = 0
+        right = len(elem)-1
+        ans = []
+        while left < right:
+          
+            curr_sum = elem[left][0] + elem[right][0]
 
-        # nums_dict = {val:index for index,val in enumerate(nums)}
-        # print(f"original dictionary: {nums_dict}")
-        # nums_tuples= list(nums_dict.keys())
-        # print(f"value of dictionary:{keys}")
-        # keys.sort()
-
-
-        # nums_tuples =[]
-        # for index, val in enumerate(nums):
-        #     nums_tuples.append((val,index))
-        # # print(nums_tuples)
-        # nums_tuples.sort()
-        # # print(nums_tuples)
-        # left = 0 
-        # right = len(nums_tuples) -1
-        # while left < right:
-        #     # print(f"left:{left},right:{right}")
-        #     current_sum = nums_tuples[left][0] + nums_tuples[right][0] 
-        #     # print(f"current_sum: {current_sum},targe:{target}")
-
-        #     if current_sum == target:
-        #         index_one = nums_tuples[left][1]
-        #         index_two = nums_tuples[right][1]
-        #         return [index_one,index_two]
-
-        #     elif current_sum < target:
-        #         left += 1
-
-        #     else:
-        #         right -= 1
-        nums_dict =dict()
-        for index, val in enumerate(nums):
-            diff = target - val
-            if diff in nums_dict:
-                return [nums_dict[diff], index]
+            if curr_sum == target:
+                ans.append(elem[left][1])
+                ans.append(elem[right][1])
+                return ans
+            if curr_sum > target:
+                right -= 1
             else:
-                nums_dict[val] = index
-            
+                left += 1
 
