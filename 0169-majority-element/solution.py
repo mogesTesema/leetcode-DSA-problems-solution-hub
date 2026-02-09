a18b0e1f4b5cee83d603b74ef4ae4154f8ecfbe4
Moges-Tesema <moges2421@gmail.com>
2026-02-09 14:35:43 +0000

    [LeetCode Sync] Runtime - 5 ms (65.30%), Memory - 21.4 MB (13.28%)

--- 0169-majority-element/solution.py
+++ 0169-majority-element/solution.py
@@ -1,22 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # count the frequency of each element in the array
-        freq_element = Counter(nums)
-       
-    
-        # assign threshold freqency to be majority element once not to recomputer on every iteration of elment while majority element checking stage.
-        majority_threshold = len(nums)/2
-        
-        # clear memory
-        del nums
+        counted_elem = Counter(nums)
+
+        for key in counted_elem:
+            if counted_elem[key] > len(nums)/2:
+                return key
 
         
-       # iterate through freqency countered elements
-        for element in freq_element:
-            # check for eligibility to be majority element
-            if freq_element[element] >= majority_threshold:
-                # return majority element you get
-                return element
             
 
 
