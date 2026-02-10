80fa39c9eedcfc6db213c27582dda51a88a54aa8
Moges-Tesema <moges2421@gmail.com>
2026-02-10 15:07:08 +0000

    [LeetCode Sync] Runtime - 47 ms (35.30%), Memory - 19.8 MB (19.89%)

--- 0599-minimum-index-sum-of-two-lists/solution.py
+++ 0599-minimum-index-sum-of-two-lists/solution.py
@@ -1,20 +1,24 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        least_indexs = 0
-        temp = 0
-        answer = []
-        for l in list1:
-            if l in list2:
-                temp = list1.index(l) + list2.index(l)
-                if temp < least_indexs:
-                    answer.clear()
-                    answer.append(l)
-                elif temp == least_indexs:
-                    answer.append(l)
-                if temp > least_indexs and len(answer) ==0:
-                    least_indexs = list1.index(l) + list2.index(l)
-                    answer.append(l)
-        return answer
 
 
+        list1_set = set(list1)
+        list2_set = set(list2)
+        common = list1_set & list2_set
+        ans = []
+        least_index = float("inf")
 
+        for chars in common:
+            index_one = list1.index(chars)
+            index_two = list2.index(chars)
+            index_sum = index_one + index_two
+
+            if index_sum < least_index:
+                ans = [chars]
+                least_index = index_one + index_two
+            elif index_sum == least_index:
+                ans.append(chars)
+        return ans
+
+
+        
