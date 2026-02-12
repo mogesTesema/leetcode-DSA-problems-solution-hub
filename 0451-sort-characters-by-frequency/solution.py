e5e3b8c6cc9163216792a9086d940b5259efccdf
Moges-Tesema <moges2421@gmail.com>
2026-02-12 12:21:06 +0000

    [LeetCode Sync] Runtime - 6 ms (84.16%), Memory - 20.2 MB (64.84%)

--- 0451-sort-characters-by-frequency/solution.py
+++ 0451-sort-characters-by-frequency/solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        str_frq = defaultdict()
-        for char in s:
-            str_frq[char] = str_frq.get(char,0) + 1
-        # print(str_frq)
+        str_freq = Counter(s)
+        freq_counted= []
+        for key,val in str_freq.items():
+            freq_counted.append([val,key])
+        
 
-        str_frq = [[frq, char] for char, frq in str_frq.items()]
-        str_frq.sort(reverse=True)
-        answer = ""
-        for frq, char in str_frq:
-            answer += char*frq
-        # print(answer)
-        return answer
+        freq_counted.sort(reverse=True)
+        ans = ""
+
+        for val,key in freq_counted:
+            ans += val*key
+        
+        return ans
+
