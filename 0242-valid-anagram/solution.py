fce6bcc1a5a704f0afaf9e51dccd56a8f5f06285
Moges-Tesema <moges2421@gmail.com>
2026-02-12 10:33:30 +0000

    [LeetCode Sync] Runtime - 4 ms (94.69%), Memory - 19.5 MB (49.57%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,18 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        count = defaultdict(int)
-        
-        # Count the frequency of characters in string s
-        for x in s:
-            count[x] += 1
-        
-        # Decrement the frequency of characters in string t
-        for x in t:
-            count[x] -= 1
-        
-        # Check if any character has non-zero frequency
-        for val in count.values():
-            if val != 0:
+
+        t_count = Counter(t)
+        s_count = Counter(s)
+
+        if len(t_count) != len(s_count):
+            return False
+        for key,val in t_count.items():
+            if key not in s_count:
                 return False
-        
+            if s_count[key] != val:
+                return False
+
         return True
+        
