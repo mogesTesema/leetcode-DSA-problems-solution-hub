0351bdb03308aa711c660fab82bc949d839bfe28
Moges-Tesema <moges2421@gmail.com>
2026-02-13 14:00:35 +0000

    [LeetCode Sync] Runtime - 3 ms (69.52%), Memory - 20.3 MB (14.12%)

---
+++ 0073-set-matrix-zeroes/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        zero_rows = set()
+        zero_cols = set()
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        for row in range(rows):
+            for col in range(cols):
+
+                if matrix[row][col] == 0:
+                    zero_rows.add(row)
+                    zero_cols.add(col)
+        
+
+
+        for row in range(rows):
+            for col in range(cols):
+
+                if row in zero_rows or col in zero_cols:
+                    matrix[row][col] = 0
+        
+    
