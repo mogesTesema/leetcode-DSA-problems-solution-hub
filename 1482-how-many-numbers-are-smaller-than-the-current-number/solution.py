4a4f96f531bfa0f70d218aa378136bc351b9009c
Moges-Tesema <moges2421@gmail.com>
2026-02-20 16:09:45 +0000

    [LeetCode Sync] Runtime - 3 ms (77.09%), Memory - 19.5 MB (22.50%)

--- 1482-how-many-numbers-are-smaller-than-the-current-number/solution.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/solution.py
@@ -1,7 +1,28 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        nums_copy = nums[:]
-        nums.sort()
-        for index in range(len(nums)):
-            nums_copy[index] = nums.index(nums_copy[index])
-        return nums_copy
+
+        index_nums = [[num,index] for index,num in enumerate(nums)]
+        index_nums.sort()
+        counter = 0
+        prev = index_nums[0][0]
+        ans = [0]*len(index_nums)
+
+
+        for i in range(len(index_nums)):
+            val = index_nums[i][0]
+            index = index_nums[i][1]
+
+            if val == prev:
+                ans[index] = counter
+                continue
+
+            ans[index] = i
+            prev = val
+            counter = i
+
+
+
+        return ans
+
+
+        
