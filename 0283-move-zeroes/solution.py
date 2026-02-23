22782d94bad0dfe481ca657670710a022b74d867
Moges-Tesema <moges2421@gmail.com>
2026-02-23 16:30:33 +0000

    [LeetCode Sync] Runtime - 8 ms (25.81%), Memory - 20.8 MB (23.28%)

--- 0283-move-zeroes/solution.py
+++ 0283-move-zeroes/solution.py
@@ -4,11 +4,18 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         holder = 0
-        seeker = 1
+        seeker = 0
+
         while seeker < len(nums):
-            if nums[holder] != 0 :
+            if nums[holder] == 0 and nums[seeker] != 0:
+                nums[holder], nums[seeker] =  nums[seeker], nums[holder]
                 holder += 1
-            if nums[seeker] != 0 and nums[holder] == 0:
-                nums[seeker], nums[holder] = nums[holder], nums[seeker]
-                holder += 1    
-            seeker += 1
+                seeker += 1
+            elif nums[seeker] == 0:
+                seeker += 1
+
+            elif nums[holder] != 0 and nums[seeker] != 0:
+                holder += 1
+                seeker += 1
+
+
