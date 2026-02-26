c398a93d880ec52c20d0e5aec75c214aedac8fda
Moges-Tesema <moges2421@gmail.com>
2026-02-26 16:55:47 +0000

    [LeetCode Sync] Runtime - 43 ms (75.14%), Memory - 30.7 MB (50.04%)

--- 2581-divide-players-into-teams-of-equal-skill/solution.py
+++ 2581-divide-players-into-teams-of-equal-skill/solution.py
@@ -2,20 +2,18 @@ class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
         skill.sort()
         left = 0
-        chemistry = 0
-        right = len(skill)-1
-        totalSkill = skill[left] + skill[right]
+        right = len(skill) -1
+        if len(skill)>=2:
+            team = skill[0]+skill[-1]
+        cem = 0
         while left < right:
-            if skill[left] + skill[right] == totalSkill:
-                chemistry += skill[left]*skill[right]
-            else:
+
+            curr_team = skill[left]+skill[right]
+            if curr_team != team:
                 return -1
+            cem += skill[left]*skill[right]
             left += 1
             right -= 1
-        return chemistry
-
-            
+        return cem
 
 
-
-        
