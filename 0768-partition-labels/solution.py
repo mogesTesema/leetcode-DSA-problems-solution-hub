be55e27832e79914bd20b1a5fdc6639adc4e6a58
Moges-Tesema <moges2421@gmail.com>
2026-02-27 17:58:24 +0000

    [LeetCode Sync] Runtime - 4 ms (39.80%), Memory - 19.4 MB (59.21%)

--- 0768-partition-labels/solution.py
+++ 0768-partition-labels/solution.py
@@ -1,31 +1,14 @@
-from collections import defaultdict
 class Solution:
-    def partitionLabels(self, s: str) -> List[int]:
-        p=defaultdict()
-        
-        for i,letter in enumerate(s):
-            p[letter]=i
-
-        farthest=0
-        i=0
-        j=0
-        res=[]
-
-        while j<len(s):
-            farthest=p[s[i]]
-            while i!=farthest:
-                i+=1
-                farthest=max(farthest,p[s[i]])
-            res.append(farthest-j+1)
-            j=i+1
-            i+=1
-
+    def partitionLabels(self, s):
+        last = {}
+        for i, c in enumerate(s):
+            last[c] = i
+        res = []
+        start = 0
+        end = 0
+        for i, c in enumerate(s):
+            end = max(end, last[c])
+            if i == end:
+                res.append(end - start + 1)
+                start = i + 1
         return res
-
-            
-
-
-
-
-
-
