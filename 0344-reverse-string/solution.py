f87ff7ae5b881da9e693dd91c521f8954214d520
Moges-Tesema <moges2421@gmail.com>
2026-03-16 16:26:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.3 MB (93.18%)

--- 0344-reverse-string/solution.py
+++ 0344-reverse-string/solution.py
@@ -6,8 +6,6 @@ class Solution:
         left = 0
         right = len(s) -1
         while left < right:
-            s[left],s[right] = s[right], s[left]
+            s[right],s[left] = s[left],s[right]
             left += 1
             right -= 1
-        
-        
