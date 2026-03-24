a58e8d88dee61d9714ee0887917db73832d0affc
Moges-Tesema <moges2421@gmail.com>
2026-03-24 15:06:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (24.65%)

---
+++ 0046-permutations/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        result = []
+
+        def backtrack(path):
+
+            if len(path) == len(nums):
+                result.append(path[:])
+
+            for num in nums:
+                if num in path:
+                    continue
+                path.append(num)
+                backtrack(path)
+                path.pop()
+
+        backtrack([])
+
+        return result
