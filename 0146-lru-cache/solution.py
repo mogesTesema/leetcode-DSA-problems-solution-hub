26dfb72b975e5a62279151710d4fcfc3408aca66
Moges-Tesema <moges2421@gmail.com>
2026-03-27 14:04:03 +0000

    [LeetCode Sync] Runtime - 135 ms (40.64%), Memory - 78.6 MB (26.03%)

--- 0146-lru-cache/solution.py
+++ 0146-lru-cache/solution.py
@@ -1,20 +1,69 @@
-from collections import OrderedDict
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
         self.capacity = capacity
+        self.lookup = {}
+
+        # Dummy head and tail
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _add_to_front(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+
+        self.head.next.prev = node
+        self.head.next = node
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key not in self.lookup:
             return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
+
+        node = self.lookup[key]
+
+        # Move to front (most recently used)
+        self._remove(node)
+        self._add_to_front(node)
+
+        return node.value
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.move_to_end(key)
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last=False)
+        if key in self.lookup:
+            # Update existing node
+            node = self.lookup[key]
+            node.value = value
 
+            # Move to front
+            self._remove(node)
+            self._add_to_front(node)
+            return
+
+        # Create new node
+        node = Node(key, value)
+        self.lookup[key] = node
+
+        self._add_to_front(node)
+
+        # Evict if needed
+        if len(self.lookup) > self.capacity:
+            # Remove least recently used (tail.prev)
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.lookup[lru.key]
